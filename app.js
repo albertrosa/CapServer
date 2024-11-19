01c53c3d198e0373a421d37bc5657e04b83ee864
albertrosa <rosalbert@gmail.com>
2024-11-19 16:33:45 -0500

    cors to domain

--- app.js
+++ app.js
@@ -1,81 +1,128 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require("passport");
-const TwitterStratergy = require("passport-twitter");
-const cors = require('cors');
-require('dotenv').config()
+// This is the Express server that will handle OAuth flow
+// and will receive the access token from Twitter
+// and will also be responsible for fetching the user's followers
+// and will be used by the client to make requests to Twitter API
+//
+// The server is created to separate the logic of handling the OAuth flow
+// and the logic of fetching the user's followers and to avoid
+// potential security issues that can arise from mixing the two
+//
+// The server is also responsible for setting up CORS to allow
+// the frontend to make requests to the server
 
-const TwitterApi = require('twitter-api-v2');
+const { Client, auth } = require("twitter-api-sdk");
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
+const cors = require("cors");
+let accessToken = "";
+dotenv.config();
 
-// import { TwitterApi } from 'twitter-api-v2';
+console.log(process.env.X_ACCOUNT);
 
-const app  = express();
-const PORT = process.env.PORT ||8080;
-
+const app = express();
 app.use(cors());
-app.use(session({
-    secret: 'asdfaeaeq25q0wedsvaf4-ta4ta32ocyn307rtap4t', // Replace with a strong secret key
-    resave: false,
-    saveUninitialized: true,
-    cookie: { 
-      secure: false, // Set to true if using HTTPS
-      maxAge: 24 * 60 * 60 * 1000 // 24 hours 
-    }
-  }));
 
-passport.use(
-    new TwitterStratergy({
-        consumerKey: process.env.CONSUMER_API_KEY,
-        consumerSecret: process.env.CONSUME_API_SECRET,
-        callbackURL: 'https://captainbeef.onrender.com/twitter/callback'
-    },
-    function(token, tokenSecret, profile, cb) {        
-        // handle user usage data
-        return cb(null, profile);
-    }
-
-));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-passport.serializeUser(function(user, cb){cb(null, user);});
-passport.deserializeUser(function(obj, cb){BaseAudioContext(null, obj);});
-
-app.get('/', (req, res)=> {
-    res.send("Hello from Identity Server");
-});
-app.get('/auth/twitter', cors(), passport.authenticate('twitter'));
-
-
-app.get('/me', cors(), async function (req, res) {
-    const auth = req.headers['authorization'];
-    console.log(auth);
-
-    const client = new TwitterApi.TwitterApi(auth);
-    const readonly = client.readOnly;
-    const user = await readonly.v2.me();
-    // const user = undefined;
-    console.log(user);
-
-
-    res.send({'name':'name', 'id': 'id', 'username':'username', 'auth': auth, "user": user});
+const authClient = new auth.OAuth2User({
+  client_id: process.env.X_ACCOUNT,
+  client_secret: process.env.X_SECRET,
+  callback: "https://captainbeef.onrender.com/twitter/callback",
+  scopes: ["tweet.read", "users.read"],
 });
 
+const client = new Client(authClient);
 
+const STATE = "my-state";
 
-app.get('/twitter/callback',function (req, res) {
-    res.redirect("/");
+app.get("/twitter/callback", async function (req, res) {
+  try {
+    const { code, state } = req.query;
+    // if (state !== STATE) return res.status(500).send("State isn't matching");
+    accessToken = (await authClient.requestAccessToken(code)).token
+      .access_token;
+    console.log("AccessToken: " + JSON.stringify(accessToken));
+
+    res.send(`
+      <html>
+      <body>
+        <p>You have been authenticated with this platform. You can close the window now.</p>
+        <script>
+          // Pass the access token and status to the parent window
+          window.opener.postMessage({ token: ${JSON.stringify(
+            accessToken
+          )}, status: "Login successful" }, "*");
+
+          // Close the window after a delay
+          setTimeout(() => {
+            window.close();
+          }, 3000); // 3 seconds delay
+        </script>
+      </body>
+      </html>
+    `);
+  } catch (error) {
+    console.log(error);
+  }
 });
-app.get('/keys', (req, res) => {
-    passport.
 
-    res.send("");
-})
+app.get("/twitter/login", async function (req, res) {
+  const authUrl = authClient.generateAuthURL({
+    state: STATE,
+    code_challenge_method: "s256",
+  });
+  res.redirect(authUrl);
+});
 
+app.get("/tweets", async function (req, res) {
+  const tweets = await client.tweets.findTweetById("20");
+  res.send(tweets.data);
+});
 
-app.listen(PORT, cors(), () => {
-    console.log(process.env.VERSION);
-    console.log("Server listenting on port " + PORT)
+app.get("/revoke", async function (req, res) {
+  try {
+    const response = await authClient.revokeAccessToken();
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/followers", async (req, res) => {
+  try {
+    // const { accessToken } = req.body;
+
+    const userResponse = await axios.get("https://api.twitter.com/2/users/me", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const userId = userResponse.data.data.id;
+    // console.log("User ID: " + userId);
+
+    // const response = await axios.get(
+    //   `https://api.twitter.com/2/users/${userId}/followers`,
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${accessToken}`,
+    //     },
+    //   }
+    // );
+
+    res.send(userResponse.data.data);
+  } catch (error) {
+    console.error("Error fetching followers:", error);
+    res.status(500).send("Failed to fetch followers");
+  }
+});
+
+app.use(
+  cors({
+    origin: "https://capserver-3eyf.onrender.com",// "https://localhost:8080",
+  })
+);
+
+app.listen(8080, () => {
+  console.log(`Go here to login: http://localhost:5000/login`);
 });
