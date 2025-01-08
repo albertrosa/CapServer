a8482f7c19e9761b96b76fd6715fee915a8a1caa
albertrosa <rosalbert@gmail.com>
2025-01-08 17:38:07 -0500

    v2 friends again

--- app.js
+++ app.js
@@ -377,7 +377,7 @@ app.get('/twitter/follows', async function (req, res){
           // console.log(followingResponse);
           // res.send(JSON.stringify(followingResponse.data));
           console.log(await client.currentUser());
-          console.log(await client.v1.friendship({source_id: xid, target_id: rid}));
+          console.log(await client.v2.followers(xid,  {max_results: 1000}));
           res.send("DONE");
           //--header 'authorization: OAuth oauth_consumer_key="CONSUMER_API_KEY", oauth_nonce="OAUTH_NONCE", oauth_signature="OAUTH_SIGNATURE", oauth_signature_method="HMAC-SHA1", oauth_timestamp="OAUTH_TIMESTAMP", oauth_token="ACCESS_TOKEN", oauth_version="1.0"' \
           
