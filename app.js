16d8868a51c732bd7a3b730967531138d1914f46
albertrosa <rosalbert@gmail.com>
2025-01-24 16:11:50 -0500

    new fields to return

--- app.js
+++ app.js
@@ -379,7 +379,11 @@ app.get('/twitter/users', async function (req, res){
           //  v2 Auth Pattern
 
           if (req.session.search != search || req.session.searchResponse == null) {
-            const searchResponse = await axios.get("https://api.x.com/2/users/by?usernames="+users+"&user.fields=created_at,name,id,profile_image_url,public_metics,username,verified,verified_followers_count&max_results=100", {
+            
+            const searchResponse = await axios.get("https://api.x.com/2/users/by?usernames="+users
+             +"&user.fields=created_at,name,id,profile_image_url,public_metics,username,verified,verified_type", {
+
+
               headers: {
                 "User-Agent": "v2UsersByJS",
                 "Content-Type": "application/json",
