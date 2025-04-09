31b1521dc6a939d0fe23f6d089e93b1b162f909e
albertrosa <rosalbert@gmail.com>
2025-04-09 13:06:39 -0400

    added Cache to Meta Lookup

--- app.js
+++ app.js
@@ -369,17 +369,24 @@ app.get('/twitter/users', async function (req, res) {
 
 app.get('/meta', async function (req, res) {
   const { send, rule } = req.query;
-  try {
-    const result = await get_meta_data(send, rule);
-    if (result) {
-      res.send(JSON.stringify({ data: result }));
-      return;
-    }
-  } catch (err) {
-    console.error(err);
-  }
 
-  res.send(JSON.stringify({ error: 'META ERROR: NO Data', login: 0 }));
+  if (req.session[rule] == null) {
+
+    try {
+      const result = await get_meta_data(send, rule);
+      if (result) {
+        res.send(JSON.stringify({ data: result }));
+        return;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    res.send(JSON.stringify({ error: 'META ERROR: NO Data', login: 0 }));
+  } else if (req.session[rule] != null) {
+    console.info("Using Session");
+    res.send(req.session[rule]);
+  }
   return;
 });
 
